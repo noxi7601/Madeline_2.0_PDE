e798e6a8b310762f1ad32ca382f3d56adeff8fef
Jangbo Bae <noxi7601@gmail.com>
2023-07-24 15:49:02 +0900

    external 을 잘못 그리는 경우 수정

--- src/NuclearFamily.cpp
+++ src/NuclearFamily.cpp
@@ -954,7 +954,7 @@ void NuclearFamily::draw(Individual* startIndividual,DrawingCanvas& dc,double st
 	//          << std::endl;
 	
 	// Draw the spouse only if the Nuclear Family is not consanguinous and does not have an external connection:
-	if(!isConsanguinous() && !hasExternalConnection()){
+	if(!isConsanguinous() && !hasExternalConnection() && !spouse->hasBeenDrawn()){
 		dc.drawIndividual(spouse,currentX+iconInterval,currentY,dashedOriginalFounder);
 	}
 	
